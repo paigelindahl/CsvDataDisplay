2fd3930e59218b566a9d4733e10348c655422c6c
Paige Lindahl <paige@peakfifteen.co.uk>
2024-04-27 21:27:15 +0100

    emit logic that passes data to csv table

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,10 +1,18 @@
 <template>
-    <CsvForm></CsvForm>
+    <CsvForm @successCsv="showData"></CsvForm>
+    <CsvTable v-show="csvData.length" :data="csvData"></CsvTable>
 </template>
 
 <script setup>
-import CsvForm from '@/Components/CsvForm.vue';
+import CsvForm from '@/Components/csv/CsvForm.vue';
+import CsvTable from '@/Components/csv/CsvTable.vue';
+import { ref } from 'vue';
 
+const csvData = ref([]);
 
+const showData = function(data) {
+    csvData.value = data.data;
+    console.log('dataaa', data.data);
+}
 
 </script>
