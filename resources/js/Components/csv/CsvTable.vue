47cd0c6c7f06f24266514117c76f7dac3b50917c
Paige Lindahl <paige@peakfifteen.co.uk>
2024-04-27 21:26:48 +0100

    adds csv table component and moves into its own csv folder

---
+++ resources/js/Components/csv/CsvTable.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <table>
+            <th>
+                <td>Customer</td>
+                <td>Site</td>
+                <td>Year</td>
+                <td>Month</td>
+                <td>Waste Type</td>
+                <td>Estimated quantity (kg)</td>
+                <td>Actual quantity (kg)</td>
+            </th>
+            <tr v-for="item in data">
+                <td>{{ item['Customer'] }}</td>
+                <td>{{ item['Site'] }}</td>
+                <td>{{ item['Year'] }}</td>
+                <td>{{ item['Month'] }}</td>
+                <td>{{item['Waste Type'] }}</td>
+                <td>{{ item['Estimated quantity (kg)'] }}</td>
+                <td>{{ item['Actual quantity (kg)']}}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<style>
+table {
+    width: 100%;
+}
+</style>
+
+<script setup>
+
+const props = defineProps({
+    data: Array,
+})
+</script>
