a287b7f53e931fb277afdd767489cdff4a73d19f
Paige Lindahl <paige@peakfifteen.co.uk>
2024-04-27 20:36:27 +0100

    csv form vue component

---
+++ resources/js/Components/CsvForm.vue
@@ -0,0 +1,34 @@
+<template>
+    <h1>Upload CSV File</h1>
+    <form enctype="multipart/form-data" @submit.prevent="uploadCsv">
+        <input type="file" name="csvFile" ref="csvInput">
+        <button type="submit">Upload</button>
+    </form>
+</template>
+
+<script setup>
+import axios from 'axios';
+import { ref } from 'vue';
+
+const csvInput = ref(null);
+
+const uploadCsv = function(e) {
+    // e.preventDefault();
+
+    const csvFile = csvInput.value.files[0];
+
+    const data = new FormData();
+    data.append('csvFile', csvFile)
+
+    axios.post(route('csv.handle'), data, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+    })
+    .then(res => {
+        console.log('it worked', res);
+    }).catch(err => {
+        console.log('err', err);
+    })
+}
+</script>
